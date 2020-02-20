24bab5ea9a5fb7a5630bbc5020c373fd8466ba75
Gustavo Lira <glira@redhat.com>
2020-02-20 17:40:54 -0300

    Updating to use infinispan-server-testdriver

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 Arquillian deploying into managed wildfly + IninfinispanServerRule running with container mode
 
 Reproducer for https://issues.redhat.com/browse/JDG-3506
-##### How to run?
-
-`mvn clean verify -Dserver.output.dir=/path/to/ispn-server -Dorg.infinispan.test.server.external=true`
--- src/test/java/com/example/ServerTest.java
+++ src/test/java/com/example/ServerTest.java
@@ -3,10 +3,8 @@ package com.example;
 import static org.junit.Assert.assertEquals;
 
 import org.infinispan.client.hotrod.RemoteCache;
-import org.infinispan.server.test.InfinispanServerRule;
-import org.infinispan.server.test.InfinispanServerRuleBuilder;
-import org.infinispan.server.test.InfinispanServerTestMethodRule;
-import org.infinispan.server.test.ServerRunMode;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.server.test.*;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
@@ -24,7 +22,16 @@ import java.nio.file.Paths;
 @RunWith(Arquillian.class)
 public class ServerTest {
 
-   private static final String ISPN_CONFIGURATION = Paths.get("src","test","resources").toAbsolutePath() + "/all.xml";
+   @ClassRule
+   public static final InfinispanServerRule SERVERS =
+           InfinispanServerRuleBuilder.config("infinispan-cluster.xml")
+                   .numServers(1)
+                   .runMode(ServerRunMode.CONTAINER)
+                   .property(TestSystemPropertyNames.INFINISPAN_TEST_SERVER_BASE_IMAGE_NAME, "infinispan/server:11.0.0.Alpha1-3")
+                   .build();
+
+   @Rule
+   public InfinispanServerTestMethodRule SERVER_TEST = new InfinispanServerTestMethodRule(SERVERS);
 
    @Deployment
    public static Archive<?> deployment() {
@@ -37,19 +44,11 @@ public class ServerTest {
                               .asFile());
    }
 
-   @ClassRule
-   public static final InfinispanServerRule SERVERS =
-         InfinispanServerRuleBuilder.config(ISPN_CONFIGURATION)
-               .numServers(1)
-               .runMode(ServerRunMode.CONTAINER)
-               .build();
 
-   @Rule
-   public InfinispanServerTestMethodRule SERVER_TEST = new InfinispanServerTestMethodRule(SERVERS);
 
    @Test
    public void simpleTest() {
-      RemoteCache<String, String> cache = SERVER_TEST.hotrod().create();
+      RemoteCache<String, String> cache = SERVER_TEST.hotrod().withCacheMode(CacheMode.DIST_SYNC).create();
       cache.put("k1", "v1");
       assertEquals(1, cache.size());
       assertEquals("v1", cache.get("k1"));
